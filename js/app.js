648b0877038e9b940bc78bea9455a8a71580a317
eliza29 <elizaseguraf@gmail.com>
2017-11-20 02:36:54 -0500

    subiendo version 0.0.3 completada

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 /* Funcionalidades
-Versión 0.0.2
-Al dar click en el botón de "Guardar", se mostrará 
-un nuevo cuadro donde estará el nombre de la lista agregada.
-Mostrar un texto de "Añadir una tarea" dentro de la lista*/
+Versión 0.0.3
+Al dar click en "Añadir una tarea", deberá mostrar
+un formulario con un textarea y un botón que diga "Añadir".*/
 
 window.addEventListener('load', function() {
     
-    var firstText = document.getElementById('first-js')
-    var container = document.getElementById('container-js') 
+    var firstText = document.getElementById('first-js');
+    var container = document.getElementById('container-js'); 
     var newDiv = document.createElement('div');
     var input = document.createElement('input');
     var button = document.createElement('button');
     var secondDiv = document.createElement('div');
     var nameInput= document.createElement('p');
     var addCard = document.createElement('p');
-    
+    var area = document.createElement('textarea');
+    var addButton = document.createElement('button');
+
     firstText.addEventListener('click',hidden)
     // ocultar texto y mostrar formulario
     function hidden(){
@@ -47,9 +48,24 @@ window.addEventListener('load', function() {
             
             addCard.textContent = 'Añadir una tarjeta ...'
             
-            secondDiv.classList.add('second-div')
-            nameInput.classList.add('name-input')
-            addCard.classList.add('add-card')
+            secondDiv.classList.add('second-div');
+            nameInput.classList.add('name-input');
+            addCard.classList.add('add-card');
             }
         }
+        
+    addCard.addEventListener('click',addArea)
+        function addArea(){
+        if(addCard){
+            secondDiv.removeChild(addCard);
+            secondDiv.appendChild(area);
+            secondDiv.appendChild(addButton);
+            
+            addButton.textContent = 'Añadir';
+            addButton.classList.add('add-button');
+            area.classList.add('style-area');
+
+            }
+        }
+    
 });
